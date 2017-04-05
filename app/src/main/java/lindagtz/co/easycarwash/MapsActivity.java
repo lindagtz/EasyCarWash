9b9095513217c88ad4b8023729b46fad0781e209
Linda Gutierrez <lindaflor_gutierrez@hotmail.com>
2017-04-05 16:03:46 -0500

    primer commit

---
+++ app/src/main/java/lindagtz/co/easycarwash/MapsActivity.java
@@ -0,0 +1,61 @@
+package lindagtz.co.easycarwash;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.Manifest;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng car = new LatLng(19.673098, -99.015353);
+        mMap.addMarker(new MarkerOptions().position(car).title("Carwash").snippet("El carwash feliz").icon(BitmapDescriptorFactory.fromResource(R.mipmap.carro)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(car));
+
+        // Assume thisActivity is the current activity
+        int permissionCheck = ContextCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionCheck==0){
+            mMap.setMyLocationEnabled(true);
+
+        }
+
+
+
+
+    }
+}
