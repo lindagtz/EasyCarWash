fc63c7c4a89ff9410395c1abf86a16519bb02bde
Linda Gutierrez <lindaflor_gutierrez@hotmail.com>
2017-08-15 23:55:43 -0500

    stars rating

--- app/src/main/java/lindagtz/co/easycarwash/InfoCarWash.java
+++ app/src/main/java/lindagtz/co/easycarwash/InfoCarWash.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -27,10 +30,15 @@ import java.net.URL;
 public class InfoCarWash extends AppCompatActivity {
 TextView direccion, telefono, horario, servicio, nombre;
 String id_auto,id_user;
+    Button SolicitarSer;
+    private static TextView text_v;
+    private static RatingBar rating_b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infocarwash);
+
 
         Bundle bundle = getIntent().getExtras();
         id_auto=bundle.getString("id_autolavado");
@@ -40,12 +48,40 @@ String id_auto,id_user;
         horario=(TextView)findViewById(R.id.txtHorario);
         servicio=(TextView)findViewById(R.id.txtServic);
         nombre=(TextView)findViewById(R.id.txtNombreAu);
+        SolicitarSer=(Button)findViewById(R.id.btnSolicitar);
+        listenerForRatingBar();
 
-
+        SolicitarSer.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(InfoCarWash.this,
+                                String.valueOf(rating_b.getRating()),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
 
 
 
     }
+
+
+
+    private void listenerForRatingBar() {
+        rating_b = (RatingBar) findViewById(R.id.ratingBar);
+        text_v = (TextView)findViewById(R.id.txtEv);
+
+        rating_b.setOnRatingBarChangeListener(
+                new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        text_v.setText(String.valueOf(rating));
+                    }
+                }
+        );
+    }
+
     @Override
     protected void onStart()
     {
